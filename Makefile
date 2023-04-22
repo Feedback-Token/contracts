3986d2cd986dc570f39d6a6a2f1b10ff0a3c9168
jonathan gregis <jon@jonathans-MacBook-Pro.local>
2023-04-22 09:57:52 -0500

    add libs

---
+++ Makefile
@@ -0,0 +1,7 @@
+-include .env
+
+init-install:
+	forge install Openzeppelin/openzeppelin-contracts foundry-rs/forge-std smartcontractkit/chainlink
+
+install:
+	forge install
--- src/Subscriptions.sol
+++ src/Subscriptions.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Subscription {}
+contract Subscription {
+    mapping(address => uint256) public balances;
+
+    constructor() {}
+
+    function funcdSubscription() public {
+        balances[msg.sender] += msg.value;
+    }
+}
